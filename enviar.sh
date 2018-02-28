b7bac33d83035f0623b6346ad12658e09d697938
AvaOs <avanildo.macedo@gmail.com>
2018-02-28 18:57:56 -0300

    Atualizacão em quarta-28/02/2018-18:57:52:57

--- enviar.sh
+++ enviar.sh
@@ -1,6 +1,7 @@
-#/bin/bash
+#!/bin/bash
 
-##comando adicional sh 
+#Programa feito para se rusado no github 26/02/2018
+#www.avanildo.com.br
 
 ## git remote add upstream [ENDEREÇO DO REPOSITÓRIO PRINCIPAL] // seta repositorio
 ## git fetch upstream   // sicroniza repositorio 
@@ -8,13 +9,13 @@
 
 
 
-data=$(date +"%d/%m/%Y")
+data=$(date +"%A-%d/%m/%Y-%X:%M")
 echo ""
 echo ""
 clear
 git status; 
 
-echo -e "\nEsta pasta e do dominio avanildo.com.br hospedado no servido openshift  \n"
+echo -e "\nEsta pasta e do dominio www.avanildo.com.br hospedado no servido GITHUB  \n"
 pwd
 echo -e "\nEste Programa foi feito para atualizacão de servidor usando Git\n \n    Aperte enter para continuar 1 para continuar \n===========================================================\n"  
 read -n 1 msg
@@ -22,14 +23,10 @@ read -n 1 msg
 
 if [ "$msg" = "1" ] 
     then
-     echo -e "\nAguarde atualizando o servidor openshift... \n \n";
-      git add . ;git commit -a -m "Atualizacão em $data ";git push; echo -e "\n \n Programa atualizado no servidor em com susseso $data \n \n" 
+     echo -e "\nAguarde atualizando o servidor Gthub... \n \n";
+      git add * ;git commit -m "Atualizacão em $data ";git push origin master ; echo -e "\n \n Programa atualizado no servidor em com susseso $data \n \n" 
    else 
       echo -e "\nAção cancelada pelo usuario obrigado"
 fi
 
 exit
-
-
-
-
