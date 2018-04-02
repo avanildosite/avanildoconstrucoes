87340c2ba7e7cf5901d1f280f2bc82fd1e50f03c
AvaOs <avanildo.macedo@gmail.com>
2018-04-02 16:34:50 -0300

    Atualizacão em segunda-02/04/2018-16:34

--- js/dados.js
+++ js/dados.js
@@ -21,23 +21,23 @@ var rodape2 = new Array();// nova array de objeto
 var blog = new Array();// nova array blog
 // Para uso do rodape
 var t1_rodape="<h2>Informações  </h2>"; // titolo do rodape
-_rodape[0]=new montamenu(dominio+"paginas/termo.html","Termo de prestação de serviço",0);
-_rodape[1]=new montamenu(dominio+"paginas/privacidade.html","Politica e Privacidade"),0; 
-_rodape[2]=new montamenu("http://avanildo.usa.cc/","Galeria de fotos",0);
-_rodape[3]=new montamenu(dominio+"paginas/nosso-clientes.html","Clientes Corporativos",0);
+	_rodape[0]=new montamenu(dominio+"paginas/termo.html","Termo de prestação de serviço",0);
+	_rodape[1]=new montamenu(dominio+"paginas/privacidade.html","Politica e Privacidade"),0; 
+	_rodape[2]=new montamenu("http://avanildo.usa.cc/","Galeria de fotos",0);
+	_rodape[3]=new montamenu(dominio+"paginas/nosso-clientes.html","Clientes Corporativos",0);
 // Ideias
 var t2_rodape="<h2>Ideias para construir </h2>";
-rodape1[0]=new montamenu("https://br.pinterest.com/avanildoconstru/cosinha/","Cozinha",0);
-rodape1[1]=new montamenu("https://br.pinterest.com/avanildoconstru/fachada-constru%C3%A7%C3%A3o-sobrado/","Fachadas",0);
-rodape1[2]=new montamenu("https://br.pinterest.com/avanildoconstru/abanheiro/","Banheiros",0);
-rodape1[3]=new montamenu("https://br.pinterest.com/avanildoconstru/salas-pequenas/","Salas"),0;
+	rodape1[0]=new montamenu("https://br.pinterest.com/avanildoconstru/cosinha/","Cozinha",0);
+	rodape1[1]=new montamenu("https://br.pinterest.com/avanildoconstru/fachada-constru%C3%A7%C3%A3o-sobrado/","Fachadas",0);
+	rodape1[2]=new montamenu("https://br.pinterest.com/avanildoconstru/abanheiro/","Banheiros",0);
+	rodape1[3]=new montamenu("https://br.pinterest.com/avanildoconstru/salas-pequenas/","Salas"),0;
 // Endereço
 var t3_rodape="<h2>Contato </h2>";
     
-rodape2[0]=new montamenu("#","Avanildo Construções",0);
-rodape2[1]=new montamenu("#","Rua:Antonio koch Leme Filho N60 ",0);    
-rodape2[2]=new montamenu("#","Fone: (19)3035-5755)",0);
-rodape2[3]=new montamenu("#","Email: Avanildocontrucoes@gmail.com",0); 
+	rodape2[0]=new montamenu("#","Avanildo Construções",0);
+	rodape2[1]=new montamenu("#","Rua:Antonio koch Leme Filho N60 ",0);    
+	rodape2[2]=new montamenu("#","Fone: (19)3035-5755)",0);
+	rodape2[3]=new montamenu("#","Email: Avanildocontrucoes@gmail.com",0); 
 
 // variavel do blog
 var blogtitolo="<h2> Informações do blog</> ";
@@ -46,23 +46,23 @@ var blogtitolo="<h2> Informações do blog</> ";
 
 
 //  Menu horizontal
-horizontal[0] = new montamenu(dominio+"index.html","Inicio",0); // monta o objeto e coloca no array
-horizontal[1] = new montamenu("http://avanildo.usa.cc","Galeria",0);     
-horizontal[2] = new montamenu("http://www.noticiascnc.com.br/","Noticias",1);     
-horizontal[3] = new montamenu(dominio+"paginas/lista-servico.html","Especialidades",0);     
-horizontal[4] = new montamenu(dominio+"paginas/contato.html","Contato",0);     
-horizontal[5] = new montamenu(dominio+"paginas/sobre.html","Sobre Nos",0);     
+	horizontal[0] = new montamenu(dominio+"index.html","Inicio",0); // monta o objeto e coloca no array
+	horizontal[1] = new montamenu("http://avanildo.usa.cc","Galeria",0);     
+	horizontal[2] = new montamenu("http://www.noticiascnc.com.br/","Noticias",1);     
+	horizontal[3] = new montamenu(dominio+"paginas/lista-servico.html","Especialidades",0);     
+	horizontal[4] = new montamenu(dominio+"paginas/contato.html","Contato",0);     
+	horizontal[5] = new montamenu(dominio+"paginas/sobre.html","Sobre Nos",0);     
 
 // Menu vertical
-vertical[0] = new montamenu(dominio+"paginas/azulejista.html","Azulejista",0);     
-vertical[1] = new montamenu(dominio+"paginas/como-construir.html","Como Construir",0);     
-vertical[2] = new montamenu(dominio+"paginas/curriculum.html","Meu Curriculum",0);     
-vertical[3] = new montamenu(dominio+"paginas/proteja-revestimento.html","Proteja Seu Revestimento ",0);
-vertical[4] = new montamenu(dominio+"paginas/forma-pagamento.html","Formas De Pagamento",0);
-vertical[5] = new montamenu("https://br.pinterest.com/avanildoconstru/","Projeto Para Inspiração",1);               
-vertical[6] = new montamenu("https://br.pinterest.com/avanildoconstru/medidas/","Medidas Para Acabamento",1);
-vertical[7] = new montamenu(dominio+"paginas/cursos.html","Cursos Gratuito Online",0);
-vertical[8] = new montamenu("javascript:void(0)","<h2> Use seu Cartão </h2> <img src='"+dominio+"img/pagamento.jpeg' width='100%' alt='Page com Visa ou Master'> <hr> Em até 12X com acrecimo <br /> Somente cartão com ship<hr>",2);
+	vertical[0] = new montamenu(dominio+"paginas/azulejista.html","Azulejista",0);     
+	vertical[1] = new montamenu(dominio+"paginas/como-construir.html","Como Construir",0);     
+	vertical[2] = new montamenu(dominio+"paginas/curriculum.html","Meu Curriculum",0);     
+	vertical[3] = new montamenu(dominio+"paginas/proteja-revestimento.html","Proteja Seu Revestimento ",0);
+	vertical[4] = new montamenu(dominio+"paginas/forma-pagamento.html","Formas De Pagamento",0);
+	vertical[5] = new montamenu("https://br.pinterest.com/avanildoconstru/","Projeto Para Inspiração",1);               
+	vertical[6] = new montamenu("https://br.pinterest.com/avanildoconstru/medidas/","Medidas Para Acabamento",1);
+	vertical[7] = new montamenu(dominio+"paginas/cursos.html","Cursos Gratuito Online",0);
+	vertical[8] = new montamenu("javascript:void(0)","<h2> Use seu Cartão </h2> <img src='"+dominio+"img/pagamento.jpeg' width='100%' alt='Page com Visa ou Master'> <hr> Em até 12X com acrecimo <br /> Somente cartão com ship<hr>",2);
 //vertical[9] = new montamenu("javascript:void(0)","Obrigado aos nossos clientes e amigos, Encerraremos nossas atividade es 22-12-17 e retornaremos em 08-01-2018. Bom descanço a todos   ","2"); 
 
 
@@ -129,14 +129,14 @@ function montarlista(m) {
 
 // variaves de apoio 
 
-telefone='<br /> (19)9-8219-3013 Tim<br />  (19)9-9556-0691 Claro';
-watsapp='<img  src="'+dominio+'img/watsap.png" align="middle"  height="100" alt="Watssap" title="WatsApp"> <br /> (19)9-8219-3013';
+	telefone='<br /> (19)9-8219-3013 Tim<br />  (19)9-9556-0691 Claro';
+	watsapp='<img  src="'+dominio+'img/watsap.png" align="middle"  height="100" alt="Watssap" title="WatsApp"> <br /> (19)9-8219-3013';
 
-aviso='<div style="color:#1f7a1f; text-align:center"> <font size="5"> <strong><p> Obrigado aos nossos clientes e amigo.'; 
-aviso+='Estamos encerrando nossas atividades apartir de';
-aviso+='16-12-2016 voltaremos em 16-01-2017, Avanildo Construções deseja a todos bom descanso de final de ano. Obrigado a todos.';
-aviso+='</p> </strong> </font>  </div>';
-logo='<div style="padding-top:25px"> ARM Construções</div>' ;
+	aviso='<div style="color:#1f7a1f; text-align:center"> <font size="5"> <strong><p> Obrigado aos nossos clientes e amigo.'; 
+	aviso+='Estamos encerrando nossas atividades apartir de';
+	aviso+='16-12-2016 voltaremos em 16-01-2017, Avanildo Construções deseja a todos bom descanso de final de ano. Obrigado a todos.';
+	aviso+='</p> </strong> </font>  </div>';
+	logo='<div style="padding-top:25px"> ARM Construções</div>' ;
 
 
 
